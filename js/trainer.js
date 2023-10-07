ca2bb71691c2a0d8ccd0441bc8bfe14766b87d37
martholomew <martholomew@users.noreply.github.com>
2023-10-07 18:14:57 +0100

    added latin verbs, cleaned things up a lil (still bad)

--- js/trainer.js
+++ js/trainer.js
@@ -12,7 +12,7 @@ async function doFunction(url, type, first_field) {
 	if (next_one === 1) {
     next_one = 0;
 
-    if (type === "noun") {
+	    if (type === "noun") {
 			let chosen_decl = [];
 			let decl_nums = ["d1", "d2", "d3", "d4", "d5"];
 			for (num of decl_nums) {
@@ -23,6 +23,23 @@ async function doFunction(url, type, first_field) {
 
 			let random_int = Math.floor(Math.random() * chosen_decl.length);
 			word = chosen_decl[random_int];
+			if (document.getElementById("minim").checked) {
+				document.getElementById("word").textContent = word["word"];
+			} else {
+				document.getElementById("word").textContent = word["word_more"];
+			}
+		}
+		else if (type === "verb") {
+			let chosen_conj = [];
+			let conj_nums = ["irr", "c1", "c2", "c3", "c4"];
+			for (num of conj_nums) {
+				if (document.getElementById(num).checked) {
+					chosen_conj = chosen_conj.concat(json[num]);
+				}
+			}
+
+			let random_int = Math.floor(Math.random() * chosen_conj.length);
+			word = chosen_conj[random_int];
 			if (document.getElementById("minim").checked) {
 				document.getElementById("word").textContent = word["word"];
 			} else {
