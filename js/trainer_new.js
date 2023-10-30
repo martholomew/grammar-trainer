d701308526d4d6d00a3d675d8cb09d1331eda10e
martholomew <martholomew@users.noreply.github.com>
2023-10-30 12:41:33 +0000

    still working on sprucing up the ui; added more irish verbs

--- js/trainer_new.js
+++ js/trainer_new.js
@@ -9,7 +9,7 @@ hidden_text = "hidden"
 function checkAnswer(element, key, value) {
 	if (element.value === value) {
 		element.style.backgroundColor = "lime";
-		element.nextElementSibling.focus();
+		focusNext();
 	} else {
 		element.style.backgroundColor = "red";
 	}
@@ -20,22 +20,22 @@ function pickWord() {
 		word = json;
 	}
 	else {
-		let chosen_conj_decl = [];
-		let conj_decl_nums = [];
-		let elements = document.getElementsByClassName("conj_decl");
+		let chosen_groups = [];
+		let groups_nums = [];
+		let elements = document.getElementsByClassName("groups");
 
 		for(let index=0; index < elements.length; index++){
-			conj_decl_nums = conj_decl_nums.concat(elements[index].id);
+			groups_nums = groups_nums.concat(elements[index].id);
 		}
 
-		for (num of conj_decl_nums) {
+		for (num of groups_nums) {
 			if (document.getElementById(num).checked) {
-				chosen_conj_decl = chosen_conj_decl.concat(json[num]);
+				chosen_groups = chosen_groups.concat(json[num]);
 			}
 		}
 
-		let random_int = Math.floor(Math.random() * chosen_conj_decl.length);
-		word = chosen_conj_decl[random_int];
+		let random_int = Math.floor(Math.random() * chosen_groups.length);
+		word = chosen_groups[random_int];
 		if (document.getElementById("minim").checked) {
 			document.getElementById("word").textContent = word["word"];
 		} else {
@@ -50,6 +50,30 @@ function pickWord() {
 }
 
 async function init(url, passed_type, passed_first_field) {
+	if (passed_type === "lat_noun") {
+		const res = await fetch("https://files.catbox.moe/pi8g0m.json");
+		const orders = await res.json();
+
+		const section = document.getElementById("section");
+		const column = document.getElementById("column");
+		const header = document.getElementById("header");
+		const conj_decl = document.getElementById("conj_decl");
+		for (const number of orders["lat_noun"]["numbers"]) {
+			let column_clone = column.content.cloneNode(true);
+			first = 1;
+			for (const form of orders["lat_noun"]["orders"]["Traditional"]) {
+				if (first === 1) {
+					let header_clone = header.content.cloneNode(true);
+					column_clone.appendChild(header_clone)
+					first = 0;
+				}
+				console.log(number, form);
+				let conj_decl_clone = conj_decl.content.cloneNode(true);
+				column_clone.appendChild(conj_decl_clone);
+			}
+			section.appendChild(column);
+		}
+	}
 	if (Object.keys(json).length === 0) {
 		const res = await fetch(url);
 		json = await res.json();
