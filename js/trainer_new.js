468dd785cea44aff8fecd173630dec118c96e037
martholomew <martholomew@users.noreply.github.com>
2023-11-02 13:49:23 +0000

    almost there!

--- js/trainer_new.js
+++ js/trainer_new.js
@@ -8,6 +8,7 @@ var enter = 0;
 var element_list = [];
 var numbers = [];
 var orders = [];
+var current_order = [];
 hidden_text = "hidden"
 
 function checkAnswer(element, key, value) {
@@ -18,6 +19,20 @@ function checkAnswer(element, key, value) {
 			if (index + 1 < element_list.length) {
 				document.getElementById(element_list[index + 1]).focus();
 			} else {
+				if (document.getElementById("next").checked) {
+					let wrong = 0;
+					for (let [key, value] of Object.entries(word["forms"])) {
+						element = document.getElementById(key);
+						if (element.value !== value) {
+							wrong = 1;
+						}
+					}
+					if (wrong === 0) {
+						enter = 1;
+						nextWord();
+						return 0;
+					}
+				}
 				document.getElementById("check").focus();
 			}
 		} else {
@@ -64,40 +79,82 @@ function pickWord() {
 
 async function init(url, passed_type) {
 	document.getElementById("check").addEventListener("click", function(){nextWord()});
-	const res = await fetch("https://martholomew.github.io/grammar-trainer/json/lang_info.json");
+	const res = await fetch("https://files.catbox.moe/bls5za.json");
 	const lang_json = await res.json();
 
-	let lang_info = lang_json[passed_type];
+	type = passed_type;
+	let lang_info = lang_json[type];
 	numbers = lang_info["numbers"];
-	orders = lang_info["orders"];
-	let default_order = lang_info["default"];
-	for (let number of numbers) {
-		for (let value of orders[default_order]) {
-			element_list.push(number + "_" + value);
+	if (lang_info["type"] === "noun") {
+		orders = lang_info["orders"];
+		let default_order = lang_info["default"];
+		current_order = orders[default_order];
+		for (let number of numbers) {
+			for (let value of current_order) {
+				if (passed_type === "sga_noun") {
+					if (number == "d" || value == "voc") {
+						element_list.push(number + "_" + value + "_part");
+						element_list.push(number + "_" + value + "_part_im");
+						element_list.push(number + "_" + value);
+						element_list.push(number + "_" + value + "_im");
+					} else {
+						element_list.push(number + "_" + value);
+						element_list.push(number + "_" + value + "_im");
+					}
+				} else {
+					element_list.push(number + "_" + value);
+				}
+			}
+		}
+
+		let radios = document.getElementsByName("order");
+		for (let radio of radios) {
+			radio.addEventListener("change", function(){changeOrder(radio.id)});
+		}
+	} else if (type === "sga_verb") {
+		let verb_forms = ["abs", "conj"];
+		for (let form of verb_forms) {
+			for (let number of numbers) {
+				for (var prs = 1; prs < 4; prs++ ) {
+					element_list.push(number + "_" + prs + "_" + form);
+				}
+			}
+		}
+	} else if (lang_info["type"] === "verb") {
+		for (let number of numbers) {
+			for (var prs = 1; prs < 4; prs++ ) {
+				element_list.push(number + "_" + prs);
+			}
 		}
 	}
-
-	let radios = document.getElementsByName("order");
-	for (let radio of radios) {
-		radio.addEventListener("change", function(){changeOrder(radio.id)});
-	}
-
 	if (Object.keys(json).length === 0) {
 		const res = await fetch(url);
 		json = await res.json();
 	}
-	type = passed_type;
 	first_field = element_list[0];
 	pickWord();
 }
 
 function changeOrder(order) {
 	element_list = [];
+	current_order = orders[order];
 	for (let number of numbers) {
 		let previous_element = document.getElementById(number + "_header_row");
-		for (let value of orders[order]) {
+		for (let value of current_order) {
 			let current_element_name = number + "_" + value + "_row";
-			element_list.push(number + "_" + value);
+			if (passed_type === "sga_noun") {
+				if ((number == "s" || number == "p") && value !== "voc") {
+					element_list.push(number + "_" + value);
+					element_list.push(number + "_" + value + "_im");
+				} else {
+					element_list.push(number + "_" + value + "_part");
+					element_list.push(number + "_" + value + "_part_im");
+					element_list.push(number + "_" + value);
+					element_list.push(number + "_" + value + "_im");
+				}
+			} else {
+				element_list.push(number + "_" + value);
+			}
 			let element = document.getElementById(current_element_name);
 			previous_element.insertAdjacentElement("afterend", element);
 			previous_element = element;
@@ -117,7 +174,7 @@ function nextWord() {
 			} else {
 				element.style.backgroundColor = "#CBFFA9";
 			}
-			if (type != "noun_sga") {
+			if (type !== "sga_noun") {
 				document.getElementById(key + "_cor").textContent = value;
 			}
 		}
@@ -127,21 +184,19 @@ function nextWord() {
 		questions += 1;
 		document.getElementById("results").textContent = right + "/" + questions;
 		if (type === "noun_sga") {
-			let cases = ["nom", "acc", "gen", "dat", "voc"];
-			let numbers = ["s", "p", "d"];
 			let answer = "";
 			let forms = word["forms"];
-				for (a_case of cases) {
-					for (number of numbers) {
-						if (number === "d" || a_case === "voc") {
-							answer = forms[number + "_" + a_case + "_part"] + "<sup>" + forms[number + "_" + a_case + "_part_im"].toUpperCase() + "</sup> " + forms[number + "_" + a_case] + "<sup>" + forms[number + "_" + a_case + "_im"].toUpperCase() + "</sup>";
-						}
-						else {
-							answer = forms[number + "_" + a_case] + "<sup>" + forms[number + "_" + a_case + "_im"].toUpperCase() + "</sup>";
-						}
-						document.getElementById(number + "_" + a_case + "_cor").innerHTML = answer;
+			for (a_case of current_order) {
+				for (number of numbers) {
+					if (number === "d" || a_case === "voc") {
+						answer = forms[number + "_" + a_case + "_part"] + "<sup>" + forms[number + "_" + a_case + "_part_im"].toUpperCase() + "</sup> " + forms[number + "_" + a_case] + "<sup>" + forms[number + "_" + a_case + "_im"].toUpperCase() + "</sup>";
 					}
+					else {
+						answer = forms[number + "_" + a_case] + "<sup>" + forms[number + "_" + a_case + "_im"].toUpperCase() + "</sup>";
+					}
+					document.getElementById(number + "_" + a_case + "_cor").innerHTML = answer;
 				}
+			}
 		}
 		enter = 1;
 	} else {
@@ -151,13 +206,11 @@ function nextWord() {
 			document.getElementById(key).style.backgroundColor = "white";
 			document.getElementById(key).value = "";
 
-			if (type != "noun_sga") {
+			if (type != "sga_noun") {
 				document.getElementById(key + "_cor").innerHTML = "<i>" + hidden_text + "</i>";
 			}
 			else {
-				let cases = ["nom", "acc", "gen", "dat", "voc"];
-				let numbers = ["s", "p", "d"];
-				for (a_case of cases) {
+				for (a_case of current_order) {
 					for (number of numbers) {
 						document.getElementById(number + "_" + a_case + "_cor").innerHTML = "<i>" + hidden_text + "</i>";
 					}
